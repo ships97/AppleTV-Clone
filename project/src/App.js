d001c30dfe8042117d3761e4f65673c439af1019
shipra singh <shiprasingh4796@gmail.com>
2022-07-22 09:38:34 +0530

    pushing in day 3

--- project/src/App.js
+++ project/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { AllRoutes } from './componants/AllRoutes/AllRoutes';
+
+import Navbar from './componants/Home/Navbar/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <AllRoutes />
     </div>
   );
 }
-
+//https://github.com/vadimghedreutan/Apple-tv-plus-clone/tree/main/src
 export default App;
